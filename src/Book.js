c7d8a5033a4868d03f0804f7b644f25112a12712
Rebekah Lane <gadgetgirlmommy@gmail.com>
2018-10-26 20:47:58 -0500

    refactor: Added additional code to prevent listing multiple authors

--- src/Book.js
+++ src/Book.js
@@ -26,7 +26,7 @@ class Book extends Component {
                     </div>
                 </div>
                 <div className="book-title">{this.props.book.title}</div>
-                <div className="book-authors">{this.props.book.authors}</div>
+                <div className="book-authors">{this.props.book.authors && this.props.book.authors[0] || "No Authors Listed"}</div>
             </div>
         )
     }
