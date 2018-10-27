48f80eb30eb189a4f0b018c7ceeb18e3dd11bf4f
Rebekah Lane <gadgetgirlmommy@gmail.com>
2018-10-26 20:15:15 -0500

    fix: Added a promise to the update() function to clear up async handling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ShowSearch from './ShowSearch'
 import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
+import Books from './Books';
 
 class BooksApp extends Component {
   state = {
@@ -17,27 +18,27 @@ class BooksApp extends Component {
   }
 
   moveShelf = (book, shelf) => {
-    BooksAPI.update(book, shelf)
-
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books: books })
-    })
+    BooksAPI.update(book, shelf).then(() => (
+      BooksAPI.getAll().then((books) => (
+        this.setState({ books: books })
+      ))
+    ))
   }
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <BookShelf
-            books={this.state.books}
-            moveShelf={this.moveShelf}
+          <BookShelf 
+          books={this.state.books}
+          moveShelf={this.moveShelf}
           />
         )} />
-        <Route path="/search" render={({ history }) => (
+        <Route path="/search" render={() => (
           <ShowSearch
-            books={this.state.books}
-            moveShelf={this.moveShelf}
-          />
+          books={this.state.books}
+          moveShelf={this.moveShelf}
+            />
         )} />
       </div>
     )
